bf0528d91fe21c0d9761f5e85ec1ab7d0061a666
ehsan <ehsansaraee6570@yahoo.com>
2021-10-28 14:03:04 +0330

    Create Sagas & rootSaga

---
+++ src/Store/Todo/sagas.ts
@@ -0,0 +1,38 @@
+import { all, call, put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+import { fetchTodoFailure, fetchTodoSuccess } from './actions';
+import { FETCH_TODO_REQUEST } from './actionTypes';
+import { ITodo } from './types';
+
+const getTodos = () =>
+   axios.get<ITodo[]>('"https://jsonplaceholder.typicode.com/todos"');
+
+// Worker Saga: Fired on FETCH_TODO_REQUEST action
+
+function* fetchTodoSaga() {
+   try {
+      const { data } = yield call(getTodos);
+      yield put(
+         fetchTodoSuccess({
+            todos: data,
+         })
+      );
+   } catch (error: unknown) {
+      if (error instanceof Error) {
+         yield put(
+            fetchTodoFailure({
+               error: error.message,
+            })
+         );
+      }
+   }
+}
+
+// Starts worker saga on latest dispatched `FETCH_TODO_REQUEST` action.
+// Allows concurrent increments.
+
+function* todoSaga() {
+   yield all([takeLatest(FETCH_TODO_REQUEST, fetchTodoSaga)]);
+}
+
+export default todoSaga;
---
+++ src/Store/rootSaga.ts
@@ -0,0 +1,6 @@
+import todoSaga from './Todo/sagas';
+import { all, fork } from 'redux-saga/effects';
+
+export function* rootSaga() {
+   yield all([fork(todoSaga)]);
+}
